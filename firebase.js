025aacb3cbedb5d0ebfcfd9e47f3bf2ea455776b
yamada1992 <85730996+yamada-1992@users.noreply.github.com>
2023-07-08 12:33:59 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmOxEgffIe8nAdQ1dlpEKcX_uK48A26FQ",
+    authDomain: "sign-in-49536.firebaseapp.com",
+    projectId: "sign-in-49536",
+    storageBucket: "sign-in-49536.appspot.com",
+    messagingSenderId: "119204202638",
+    appId: "1:119204202638:web:a57f474bd01859c4da21df",
+    measurementId: "G-JJ3E7FZRMQ"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
